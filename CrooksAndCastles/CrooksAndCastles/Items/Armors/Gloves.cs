ca698eeac8aef3b23b9612e18a2cc044cdc914e7
Viktor Terziev <terzievviktor@gmail.com>
2015-06-16 20:48:11 +0300

    Added: Weapon potions and armor Somebody needs to add images for these items

---
+++ CrooksAndCastles/CrooksAndCastles/Items/Armors/Gloves.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+namespace DrunkenSoftUniWarrior.Items.Armors
+{
+    internal class Gloves : Armor
+    {
+        private const string path = "";
+
+        public Gloves(Point position, int level)
+            :base(position, level)
+        {
+            this.Picture = new Bitmap(path);
+            this.Defence = 0.3 * this.Level;
+            this.ItemStats.Image = this.Picture;
+            this.ItemButton.Image = resizeImage(this.Picture, new Size(ItemButtonSize, ItemButtonSize));
+            this.ItemStats.Text = string.Format("{0}\n\nDefence: {1}\n\n Level: {2}", this.GetType().Name, this.Defence.ToString(), this.Level);
+        }
+    }
+}
---
+++ CrooksAndCastles/CrooksAndCastles/Items/Weapons/Axe.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace DrunkenSoftUniWarrior.Items.Weapons
+{
+    internal class Axe : Weapon
+    {
+        private const string Path = "";
+
+        public Axe(Point position, int level)
+            : base(position, level)
+        {
+            this.Picture = new Bitmap(Path);
+            this.Damage = 0.5 * this.Level;
+            this.ItemStats.Image = this.Picture;
+            this.ItemButton.Image = resizeImage(this.Picture, new Size(ItemButtonSize, ItemButtonSize));
+            this.ItemStats.Text = string.Format("{0}\n\nDamage: {1}\n\n Level: {2}", this.GetType().Name, this.Damage.ToString(), this.Level);
+        }
+    }
+}
