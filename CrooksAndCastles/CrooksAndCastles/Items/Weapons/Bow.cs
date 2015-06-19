4a1de1f99957704fb89f125c59144b3a9a659f02
DimitrovZ <zlatin.dimitrov84@gmail.com>
2015-06-19 05:06:06 +0300

    Added items

---
+++ CrooksAndCastles/CrooksAndCastles/Items/Weapons/Bow.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace DrunkenSoftUniWarrior.Items.Armors
+{
+    internal class Bow : Armor
+    {
+        private const string Path = "Bow.jpg";
+
+        public Bow(Point position, int level)
+            : base(position, level)
+        {
+            this.Picture = new Bitmap(Path);
+            this.Defence = 0.5 * this.Level;
+            this.ItemStats.Image = this.Picture;
+            this.Image = resizeImage(this.Picture, new Size(ItemButtonSize, ItemButtonSize));
+            this.ItemStats.Text = string.Format("{0}\n\nDefence: {1}\n\n Level: {2}", this.GetType().Name, this.Defence.ToString(), this.Level);
+        }
+    }
+}
